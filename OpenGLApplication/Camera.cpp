d76219e6a77253fec5ca8458692e9e1c53826a43
Paul King <PaulKing09@bigpond.com>
2022-03-29 15:47:27 +1100

    Updated camera class and added Bootstrap input class

--- OpenGLApplication/Camera.cpp
+++ OpenGLApplication/Camera.cpp
@@ -1,12 +1,18 @@
 #include "Camera.h"
 #include <ext/matrix_clip_space.hpp>
+#include "Input.h"
+
+Camera::Camera(int width, int height) : shader(new ShaderProgram()), windowWidth(width), windowHeight(height)
+{
+
+}
 
 mat4 Camera::GetViewMatrix()
 {
-	float thetaR = glm::radians(theta);
-	float phiR = glm::radians(phi);
+	float thetaR = glm::radians(m_theta);
+	float phiR = glm::radians(m_phi);
 	vec3 forward(cos(phiR) * cos(thetaR), sin(phiR), cos(phiR) * sin(thetaR));
-	return glm::lookAt(position, position + forward, glm::vec3(0, 1, 0));
+	return glm::lookAt(m_position, m_position + forward, glm::vec3(0, 1, 0));
 }
 
 mat4 Camera::GetProjectionMatrix(float w, float h)
@@ -14,7 +20,42 @@ mat4 Camera::GetProjectionMatrix(float w, float h)
 	return glm::perspective(glm::pi<float>() * 0.25f, w / h, 0.1f, 1000.f);
 }
 
-void Camera:: Update()
+void Camera::Update(float deltaTime)
 {
+	// bind transform
+	glm::mat4 projectionMatrix = GetProjectionMatrix(windowWidth, windowHeight);
+	glm::mat4 viewMatrix = GetViewMatrix();
+	auto pvm = projectionMatrix * viewMatrix;// *m_bunnyTransform;
 
+	shader->SetUniform("mvp", projection * view * rotation);
+	
+	////////////////////////////////
+	// Camera movement from input //
+	////////////////////////////////
+	aie::Input* input = aie::Input::getInstance();
+	float thetaR = glm::radians(m_theta);
+	float phiR = glm::radians(m_phi);
+
+	//calculate the forwards and right axes and up axis for the camera
+	glm::vec3 forward(cos(phiR) * cos(thetaR), sin(phiR), cos(phiR) * sin(thetaR));
+	glm::vec3 right(-sin(thetaR), 0, cos(thetaR));
+	glm::vec3 up(0, 1, 0);
+
+	// use WASD, ZX keys to move camera around
+	if (input->isKeyDown(aie::INPUT_KEY_X))
+		m_position += up * deltaTime;
+
+	// get the current mouse coordinates
+	float mx = input->getMouseX();
+	float my = input->getMouseY();
+	const float turnSpeed = 0.1f;
+	// if the right button is down, increment theta and phi
+	if (input->isMouseButtonDown(aie::INPUT_MOUSE_BUTTON_RIGHT))
+	{
+		m_theta += turnSpeed * (mx - m_lastMouseX);
+		m_phi -= turnSpeed * (my - m_lastMouseY);
+	}
+	// store this frames values for next frame
+	m_lastMouseX = mx;
+	m_lastMouseY = my;
 }
--- OpenGLApplication/Camera.h
+++ OpenGLApplication/Camera.h
@@ -1,21 +1,34 @@
 #pragma once
 #include <glm.hpp>
 #include <ext/matrix_transform.hpp>
+#include "ShaderProgram.h"
 
 using namespace glm;
 
 class Camera
 {
 public:
+
+	Camera(int width, int height);
+	~Camera() { delete shader; }
 	mat4 GetViewMatrix();
 	mat4 GetProjectionMatrix(float w, float h);
-	void Update();
+	void Update(float deltaTime);
 
-private:
-	float theta;
-	float phi;
-	vec3 position;
+private:	
+	
 	mat4 rotation;
 	mat4 view;
 	mat4 projection;
+
+	int windowWidth, windowHeight;
+
+	ShaderProgram* shader;
+
+	// camera position and angle
+	vec3 m_position;
+	float m_theta;
+	float m_phi;
+	float m_lastMouseX;
+	float m_lastMouseY;
 };
